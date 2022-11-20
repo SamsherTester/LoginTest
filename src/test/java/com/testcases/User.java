2be4475d267f3527b76a4e41343d3bed0427e934
samshertester <samshertester@gmail.com>
2022-11-20 14:30:46 +0530

    updated toString()

--- src/test/java/com/testcases/User.java
+++ src/test/java/com/testcases/User.java
@@ -1,41 +1,37 @@
 package com.testcases;
 
-
 public class User {
-	
+
 	private String uName;
 	private String pwd;
-	private String id;
-	
 	private String age;
-	
-	
+
 	public String getuName() {
 		return uName;
 	}
+
 	public void setuName(String uName) {
 		this.uName = uName;
 	}
+
 	public String getPwd() {
 		return pwd;
-	}public void setPwd(String pwd) {
+	}
+
+	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
-	
-	
 
-	@Override
-	public String toString() {
-		return "User [uName=" + uName + ", pwd=" + pwd + "]";
-	}
 	public String getAge() {
 		return age;
 	}
+
 	public void setAge(String age) {
 		this.age = age;
 	}
-	
-	
-	
 
+	@Override
+	public String toString() {
+		return "User [uName=" + uName + ", pwd=" + pwd + ", age=" + age + "]";
+	}
 }
