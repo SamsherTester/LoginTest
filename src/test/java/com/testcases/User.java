58ada435f29e8309b716913670130616eb273657
samshertester <samshertester@gmail.com>
2022-11-20 13:57:38 +0530

    created User class

---
+++ src/test/java/com/testcases/User.java
@@ -0,0 +1,20 @@
+package com.testcases;
+
+public class User {
+	
+	private String uName;
+	private String pwd;
+	public String getuName() {
+		return uName;
+	}
+	public void setuName(String uName) {
+		this.uName = uName;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+}
